35b53bb4e3e31b0af760fad5361635c164c9ecc1
Faizan Khan <fk790080@gmail.com>
2025-03-12 04:27:15 +0530

    Working on Frontend

--- src/app.js
+++ src/app.js
@@ -5,10 +5,20 @@ const path = require("path");
 const app = express();
 const weatherData = require("../utils/weatherData");
 
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(publicPath));
+
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, this is my first express app.");
+    res.render("index", {title: "Weather App"});
 });
 
 app.get("/weather", (req, res) => {
@@ -21,6 +31,10 @@ app.get("/weather", (req, res) => {
         }
         res.send(result);
     });
+});
+
+app.get("*", (req, res) => {
+    res.render("404", {title: "Page not found"});
 });
 
 app.listen(port, () => {
